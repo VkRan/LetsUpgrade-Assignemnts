32437cb8a8b38aa0404d4354d7ddc1a698b810ab
Vikalp Shishodia <vikalpshishodia37@gmail.com>
2021-01-26 03:38:42 +0530

    Assignment 2

---
+++ Assignment 2/Q2.js
@@ -0,0 +1,8 @@
+//Question 2
+//Write a Javascript program to find the sum of all elements of a given array
+var arr=[10,21,30,43,9,40]
+var sum=0
+for(var num=0; num<arr.length;num++){
+   sum+=arr[num];
+}
+console.log(result)
